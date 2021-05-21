c896c47f56361ba69d8773b96344a24327ef32a3
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-21 15:17:02 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    #新規登録時のストロングパラメータに「追加したいカラム」を設定
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+  end
 end
